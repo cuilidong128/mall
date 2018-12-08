411a69beeddbba2955cce46f2dc5cb416a260951
cuilidong128 <mrcuilidong@163.com>
2018-12-09 01:20:27 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mall
 商城系统
+
+elk-mall-common 通用模块Base项目
+elk-mall-config 加载配置
---
+++ elk-mall-common/src/main/java/com/mall/common/aspect/RpcLogAspect.java
@@ -0,0 +1,7 @@
+package com.mall.common.aspect;
+
+/**
+ * Created by cuilidong on 2018/12/9.
+ */
+public class RpcLogAspect {
+}
