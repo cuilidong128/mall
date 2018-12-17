6a9c03d331b3c9806b7b805596a3bbc20b295e0f
cuilidong128 <mrcuilidong@163.com>
2018-12-17 23:43:39 +0800

    no message

--- elk-mall-module/elk-mall-system/src/main/java/com/mall/system/Application.java
+++ elk-mall-module/elk-mall-system/src/main/java/com/mall/system/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -18,6 +19,7 @@ import java.time.LocalDateTime;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
+@EnableAutoConfiguration
 @MapperScan(value = "com.mall.system.dao")
 @ComponentScan(basePackages = "com.mall.system,com.mall.common.base.service") //com.mall.common.base.service
 public class Application extends SpringBootServletInitializer {
